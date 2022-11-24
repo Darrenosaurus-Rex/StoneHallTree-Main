8008ab37169059664ff8a2a335213edb99652448
Darrenosaurus-Rex <118442861+Darrenosaurus-Rex@users.noreply.github.com>
2022-11-24 20:37:02 +0000

    Initial commit

---
+++ data.js
@@ -0,0 +1,62 @@
+export const creatorDetails = {
+    accountHandle: "@StoneHallCards",
+    creatorName: "",
+    tagline: "The Future of Banking and Business",
+    profilePic: "SHCLogo.png",
+  };
+  
+  export const socialLinks = [
+    {
+      url: "https://www.tiktok.com/@stonehallcards",
+      iconName: "tiktok",
+      active: true,
+    },
+    {
+      url: "https://www.facebook.com/stonehallcards",
+      iconName: "facebook",
+      active: true,
+      },
+    {
+      url: "https://www.youtube.com/channel/UCtvDq9lVcnKTJd3dugz6xJw",
+      iconName: "youtube",
+      active: true,
+    },
+    {
+      url: "",
+      iconName: "twitter",
+      active: false,
+    },
+    {
+      url: "https://ie.linkedin.com/in/sara-rogantoni-038a61170",
+      iconName: "linkedin",
+      active: true,
+    },
+    {
+      url: "https://www.instagram.com/stonehallcards/?igshid=YmMyMTA2M2Y%3D",
+      iconName: "instagram",
+      active: true,
+    },
+  ];
+  
+  export const whereNextLinks = [
+    {
+      url: "https://stonehallcards.com/",
+      text: "Vist Our Website",
+      active: true,
+    },
+    {
+      url: "mailto:sara@stonehallcards.com",
+      text: "Send Us An Email",
+      active: true,
+    },
+    {
+      url: "tel:0851859524",
+      text: "Give Us A Call",
+      active: true,
+    },
+    {
+      url: "tel:0851859524",
+      text: "Give Us A Call",
+      active: true,
+    },
+  ];
